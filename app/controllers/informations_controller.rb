aef9cd99dd03f8e193bf8ed0670fbd63e4a12e7c
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-19 19:25:59 +0900

    informtion ストロングパラメータ追加

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -7,5 +7,9 @@ class InformationsController < ApplicationController
   def new
   end
 
-  
+  private
+
+  def information_params
+    params.require(:information).permit(:image, :text, :date_id, :fish, :size, :weight, :number, :prefecture_id, :area).merge(user_id: current_user.id)
+  end
 end
