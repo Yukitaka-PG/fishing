ff3b5c5fead32d49c01876d320ae92b5b4d155c5
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-25 12:40:16 +0900

    views.indexトップページ変更.new追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'informations#index'
-  resources :informations, only: [:index]
+  resources :informations, only: [:index, :new]
   
 end
