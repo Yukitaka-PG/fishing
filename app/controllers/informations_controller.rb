6db93f524f5192507ee4e1349283a7a3da4cb5cb
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-28 12:41:40 +0900

    投稿してトップページに表示

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -9,6 +9,15 @@ class InformationsController < ApplicationController
     @information = Information.new
   end
 
+  def create
+    @information = Information.new(information_params)
+    if @information.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def information_params
--- app/models/information.rb
+++ app/models/information.rb
@@ -2,16 +2,18 @@ class Information < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-
   with_options presence: true do
     validates :image
-    validates :date_id
+    validates :title
     validates :fish
+    validates :date_id
   end
 
   with_options numericality: { other_than: 1 } do
     validates :prefecture_id
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
 end
