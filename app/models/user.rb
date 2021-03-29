02ad6bce70fb72370d1c5f8157df79ec7cd33909
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-29 18:42:59 +0900

    ログイン画面修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,6 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "全角文字を使用してください"}
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "全角文字を使用してください"}
-    validates :first_name_katakana, format: { with: /\A[ァ-ヶ一]+\z/, message: "全角カタカナを使用してください"}
-    validates :family_name_katakana, format: { with: /\A[ァ-ヶ一]+\z/, message: "全角カタカナを使用してください"}
 
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
