bd1a62494775774645834383c2d1f17a032e75eb
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-31 21:33:23 +0900

    information単体テストコード実装

--- spec/factories/information.rb
+++ spec/factories/information.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :information do
-    
+    title { 'タイトル' }
+    text { 'テキスト' }
+    date_id { 20210401 }
+    fish { '魚' }
+    size { 1 }
+    weight { 1 }
+    number { 1 }
+    prefecture_id { 2 }
+    area { '地域' }
+
+    after(:build) do |information|
+      information.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
