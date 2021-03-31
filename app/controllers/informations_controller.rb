517f1d574c6b0a702b1804558a7a476fe0a65023
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-31 14:38:48 +0900

    README修正２

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,5 +1,5 @@
 class InformationsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @informations = Information.all.order("created_at DESC")
