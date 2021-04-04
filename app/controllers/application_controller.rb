c54ca551f2b781068f72efd5e2e43e39638ec959
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-04-04 09:15:33 +0900

    sign_up ネーム削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_katakana, :family_name_katakana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
